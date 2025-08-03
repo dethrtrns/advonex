f5b330f956e152792603c5d02fdf4e1f2e2b70f5
dethrtrns <rajshekharsingh060593@gmail.com>
2025-08-03 14:07:58 +0530

    cn function moved to lib dir

--- src/components/ui/liquid-glass-card.tsx
+++ src/components/ui/liquid-glass-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { cn } from "@/lib/common/utils";
+import { cn } from "@/lib/utils";
 
 const LiquidGlassCard = React.forwardRef<
   HTMLDivElement,

0ce330c753ea6fd686dbccdb1ec0cd6877d9ad43
dethrtrns <rajshekharsingh060593@gmail.com>
2025-08-13 17:17:26 +0530

    added SwipableSidebar component from another project to test and implement parts in existing sidebar.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,12 @@
 import { RegisterDialog } from "@/components/auth/register-dialog";
+import { SwipableSidebar } from "@/components/layout/SwipableSidebar";
 import { LoginModal } from "@/hooks/login/login-modal";
 
 export default function Login() {
-  return <div>{/* <LoginModal /> */}</div>;
+  return (
+    <div>
+      {/* <LoginModal /> */}
+      <SwipableSidebar />
+    </div>
+  );
 }

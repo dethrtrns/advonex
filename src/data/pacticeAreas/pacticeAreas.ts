5d5c4d466299229b43a91867084a5163df87d786
dethrtrns <rajshekharsingh060593@gmail.com>
2025-08-19 21:02:52 +0530

    so trying static data init

--- src/data/pacticeAreas/pacticeAreas.ts
+++ src/data/pacticeAreas/pacticeAreas.ts
@@ -1,3 +1,38 @@
+// import { toast } from "sonner";
+
+// export type practiceArea = {
+//   id: string;
+//   name: string;
+//   description: string;
+// };
+
+// export async function practiceAreas(): Promise<practiceArea[]> {
+//   try {
+//     const response = await fetch(
+//       `${process.env.NEXT_PUBLIC_BASE_BACKEND_URL}/static-data/practice-areas`,
+//       {
+//         method: "GET",
+//       }
+//     );
+
+//     const responseData = await response.json();
+
+//     if (!response.ok) {
+//       throw new Error(responseData.message || "Failed to update profile");
+//     }
+
+//     return responseData.data;
+//   } catch (error) {
+//     console.error("Error getting practice areas:", error);
+//     toast.error(
+//       error instanceof Error
+//         ? error.message
+//         : "An unknown error occurred during profile update."
+//     );
+//     throw error;
+//   }
+// }
+
 export const practiceAreas = [
   "Civil Law",
   "Criminal Law",
